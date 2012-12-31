208816a0ba57f7522ce69e2af86a98111884e85f
Robert Rose <rrose1@indiana.edu>
2012-12-31 15:16:38 -0500

    Added subclass `TermValueArray` of numpy.ndarray that handles the pretty-printing of typical vsm data structures. Updated `*similar_*` functions to use it

--- vsm/viewer/__init__.py
+++ vsm/viewer/__init__.py
@@ -84,6 +84,12 @@ def similar_terms(corpus, matrix, term,
 
         sim_vals = sim_vals[f(sim_vals['term'])]
 
+    sim_vals = sim_vals.view(TermValueArray)
+
+    sim_vals.main_header = term
+
+    sim_vals.subheaders = [('Term', 'Cosine')]
+
     return sim_vals
 
 
@@ -103,6 +109,12 @@ def similar_documents(corpus, matrix, tok_name, doc_query,
     sim_vals = np.array([(docs[i], v) for i,v in sim_vals],
                         dtype=[('doc', docs.dtype),
                                ('value', sim_vals.dtype['v'])])
+
+    sim_vals = sim_vals.view(TermValueArray)
+
+    sim_vals.main_header = docs[doc_query]
+
+    sim_vals.subheaders = [('Document', 'Cosine')]
 
     return sim_vals
 
@@ -147,4 +159,149 @@ def mean_similar_terms(corpus, matrix, query,
 
         sim_vals = sim_vals[f(sim_vals['term'])]
 
+    sim_vals = sim_vals.view(TermValueArray)
+
+    sim_vals.main_header = query
+
+    sim_vals.subheaders = [('Term', 'Cosine')]
+
     return sim_vals
+
+
+
+class TermValueArray(np.ndarray):
+    """
+    """
+    def __new__(cls, input_array, main_header=None, subheaders=None):
+
+        obj = np.asarray(input_array).view(cls)
+
+        obj.main_header = main_header
+
+        obj.subheaders = subheaders
+
+        return obj
+
+
+
+    def __array_finalize__(self, obj):
+
+        if obj is None: return
+
+        self.main_header = getattr(obj, 'main_header', None)
+
+        self.subheaders = getattr(obj, 'subheaders', None)
+
+
+
+    def __str__(self):
+
+        if self.ndim == 1:
+
+            arr = self[np.newaxis, :]
+
+        elif self.ndim == 2:
+
+            arr = self
+
+        else:
+
+            return super(TermValueArray, self).__str__()
+
+        vsep_1col = '-' * 30 + '\n'
+
+        vsep_2col = '-' * 65 + '\n'
+
+        if arr.main_header:
+        
+            if arr.shape[0] == 1:
+                
+                s = vsep_1col
+
+                s += '{0:^30}\n'.format(arr.main_header)
+
+            else:
+
+                s = vsep_2col
+
+                s += '{0:^65}\n'.format(arr.main_header)
+
+        else:
+
+            s = ''
+
+        m, n = arr.shape[0], arr.shape[1]
+
+        for i in xrange(0, m - m % 2, 2):
+
+            if arr.subheaders:
+
+                s += vsep_2col
+
+                s += ('{0:<20}{1:<15}{2:<20}{3}\n'
+                      .format(arr.subheaders[i][0], 
+                              arr.subheaders[i][1],
+                              arr.subheaders[i+1][0], 
+                              arr.subheaders[i+1][1]))
+                                      
+            s += vsep_2col
+
+            for j in xrange(n):
+
+                s += ('{0:<20}{1:<15.5f}{2:<20}{3:.5f}\n'
+                      .format(arr[i][j][0], arr[i][j][1], 
+                              arr[i+1][j][0], arr[i+1][j][1]))
+
+        if m % 2:
+
+            if arr.subheaders:
+
+                s += vsep_1col
+
+                s += ('{0:<20}{1}\n'
+                      .format(arr.subheaders[m-1][0], 
+                              arr.subheaders[m-1][1]))
+                                      
+            s += vsep_1col
+
+            for j in xrange(n):
+
+                s += ('{0:<20}{1:<15.5f}\n'
+                      .format(arr[m-1][j][0], arr[m-1][j][1]))
+            
+        return s
+
+
+
+
+def test_TermValueArray():
+
+    terms = ['row', 'row', 'row', 'your', 'boat', 'gently', 'down', 'the', 
+             'stream', 'merrily', 'merrily', 'merrily', 'merrily', 'life', 
+             'is', 'but', 'a', 'dream']
+
+    values = [np.random.random() for t in terms]
+
+    d = [('term', np.array(terms).dtype), 
+         ('value', np.array(values).dtype)]
+
+    v = np.array(zip(terms, values), dtype=d)
+
+    arr = np.vstack([v] * 5)
+
+    arr = arr.view(TermValueArray)
+
+    arr.main_header = 'Test 2-d Array'
+
+    arr.subheaders = [('Repetition ' + str(i), 'Random') 
+                      for i in xrange(arr.shape[0])]
+    
+    print arr
+
+    print
+
+    arr = v.view(TermValueArray)
+
+    arr.main_header = 'Test 1-d Array'
+    
+    print arr
