c649a8ea89578eb7ab082555536524bee2bd6c9e
Robert Rose <rrose1@indiana.edu>
2013-01-01 17:04:41 -0500

    Added `map_strarr` function, which generalizes the application of int->term maps to term-value arrays.

--- vsm/__init__.py
+++ vsm/__init__.py
@@ -24,6 +24,41 @@ def enum_sort(a):
 
 
 
+def map_strarr(arr, m, k, new_k=None):
+    """
+    Takes a structured array `arr`, a field name `k` and an Indexable
+    `m` and returns a copy of `arr` with its field `k` values mapped
+    according to `m`. If `new_name` is given, the field name `k` is
+    replaced with `new_name`.
+    """
+    if not new_k:
+
+        new_k = k
+
+    k_vals = np.array([m[i] for i in arr[k]])
+
+    dt = [(n, arr.dtype[n]) for n in arr.dtype.names]
+
+    i = arr.dtype.names.index(k)
+
+    dt[i] = (new_k, k_vals.dtype)
+
+    new_arr = np.empty_like(arr, dtype=dt)
+
+    for n in new_arr.dtype.names:
+        
+        if n == new_k:
+
+            new_arr[new_k][:] = k_vals[:]
+
+        else:
+
+            new_arr[n][:] = arr[n][:]
+
+    return new_arr
+
+
+
 def row_norms(matrix):
 
     norms = np.empty(matrix.shape[0])
@@ -102,4 +137,19 @@ def test_row_normalize():
 
     m = row_normalize(m)
 
-    print np.allclose(row_norms(m), np.ones(5))
+    assert np.allclose(row_norms(m), np.ones(5))
+
+
+
+def test_map_strarr():
+
+    arr = np.array([(0, 1.), (1, 2.)], 
+                   dtype=[('i', 'i4'), ('v', 'f4')])
+
+    m = ['foo', 'bar']
+
+    arr = map_strarr(arr, m, 'i', new_k='str')
+
+    assert (arr['str'] == np.array(m, dtype=np.array(m).dtype)).all()
+
+    assert (arr['v'] == np.array([1., 2.], dtype='f4')).all()
