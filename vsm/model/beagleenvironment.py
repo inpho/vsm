ed9854f18bafc6686b5c7be9818bc63503758279
Robert Rose <rrose1@indiana.edu>
2013-01-20 12:17:54 -0500

    BeagleEnvironment updated. Test added.

--- vsm/model/beagleenvironment.py
+++ vsm/model/beagleenvironment.py
@@ -1,95 +1,41 @@
-import pickle
-
 import numpy as np
 
-from vsm import model
+from vsm import row_normalize
 
 
 
-class BeagleEnvironment(model.Model):
+class BeagleEnvironment(object):
 
-    def train(self, corpus, n_columns=2048, dtype=np.float32, rand_pool=None):
-
-
-        if rand_pool:
-
-            shape = (corpus.terms.shape[0], n_columns)
-                        
-            self.matrix = np.empty(shape, dtype=dtype)
-            
-            for i in xrange(shape[0]):
-
-                v = rand_pool.request_vector(corpus.terms[i])
-
-                self.matrix[i, :] = v[:]
-                
-
-        else:
-
-            shape = (corpus.terms.shape[0], n_columns)
-
-            self.matrix = np.asarray(np.random.random(shape), dtype=dtype)
-
-            self.matrix *= 2
-
-            self.matrix -= 1
-            
-            norms = np.sum(self.matrix**2, axis=1)**(.5)
-            
-            self.matrix /= norms[:, np.newaxis]
-
-
-
-class RandomVectorPool(object):
-    """
-    """
-    def __init__(self, seed=None, n_columns=2048, dtype=np.float32):
-
-        self.n_columns = n_columns
-
+    def __init__(self, corpus, n_cols=2048, dtype=np.float64):
+        """
+        """
+        self.shape = (corpus.terms.shape[0], n_cols)
         self.dtype = dtype
 
-        self.rstate = np.random.mtrand.RandomState()
 
-        self.rstate.seed(seed)
-
-        self.terms = []
-
-        self.matrix = np.empty(0)
-
-
-
-    def request_vector(self, term):
-
-        if term in self.terms:
-
-            return self.matrix[self.terms.index(term)]
-
-        else:
-
-            v = np.asarray(self.rstate.rand(self.n_columns),
-                           dtype=self.dtype)
-
-            v = v * 2 - 1
-
-            v = v / np.dot(v, v)**.5
-
-            if self.matrix.any():
-                
-                self.matrix = np.vstack([self.matrix, v])
-
-            else:
-
-                self.matrix = v[np.newaxis, :]
-
-            self.terms += [term]
-
-            return v
+    def train(self):
+        """
+        """
+        self.matrix = np.array(np.random.random(self.shape), 
+                               dtype=self.dtype)
+        self.matrix = self.matrix * 2 - 1
+        self.matrix = row_normalize(self.matrix)
 
 
 
-    def save(self, filename):
+def test_BeagleEnvironment():
 
-        with open(filename, 'w') as f:
+    from vsm.util.corpustools import random_corpus
+    from vsm import row_norms
 
-            pickle.dump(self, f)
+    c = random_corpus(1000, 100, 0, 20)
+
+    m = BeagleEnvironment(c, n_cols=100)
+    m.train()
+
+    assert (m.matrix <= 1).all()
+    assert (m.matrix >= -1).all()
+
+    norms = row_norms(m.matrix)
+
+    assert np.allclose(np.ones(norms.shape[0]), norms)
