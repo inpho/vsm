1172ca6052544696445a6c50aa26b86016ba8b9d
Robert Rose <rrose1@indiana.edu>
2013-01-20 16:44:21 -0500

    beagleorder.py updated.

--- vsm/model/beagleorder.py
+++ vsm/model/beagleorder.py
@@ -2,29 +2,26 @@ import os
 import shutil
 import tempfile
 import multiprocessing as mp
+import cPickle as cpickle
 
 import numpy as np
 from numpy import dual
 
-from vsm import rand_pt_unit_sphere, naive_cconv
-from vsm import corpus as corp
-from vsm import model
-from vsm.model import beagleenvironment as be
+from vsm import rand_pt_unit_sphere
 
 
 
 def two_rand_perm(n, seed=None):
-
+    """
+    """
     np.random.seed(seed)
     
     perm1 = np.random.permutation(n)
 
     while True:
-
         perm2 = np.random.permutation(n)
 
         if not (perm2 == perm1).all():
-
             break
         
     return perm1, perm2
@@ -32,15 +29,14 @@ def two_rand_perm(n, seed=None):
 
 
 def mk_b_conv(n, rand_perm=None):
-
+    """
+    """
     if rand_perm is None:
-
         rand_perm = two_rand_perm(n)
     
     def b_conv(v1, v2):
         
         w1 = dual.fft(v1[rand_perm[0]])
-
         w2 = dual.fft(v2[rand_perm[1]])
 
         return np.real_if_close(dual.ifft(w1 * w2))
@@ -57,25 +53,18 @@ def ngram_slices(i, n, l):
     out = []
 
     a = i - n + 1
-
     if a < 0:
-
         a = 0
 
     b = i + 1
-
     if b + n > l:
-
         b = l - n + 1
 
     d = b - a
 
     for k in xrange(d):
-
         start = a + k
-
         stop = start + n
-            
         out.append(slice(start, stop))
 
     return out
@@ -96,265 +85,163 @@ def reduce_ngrams(fn, a, n, i, flat=True):
     for j in xrange(2, m + 1):
 
         slices = ngram_slices(i, j, a.shape[0])
-
         init = slices[0]
-
         out[j] = { init.start: reduce(fn, a[init]) }
 
         for s in slices[1:]:
-
             prev = out[j - 1][s.start]
-
             out[j][s.start] = fn(prev, a[s.stop - 1])
 
     # Delete the 1-gram
-
     del out[1]
 
     if flat:
-
         out = [v for d in out.values() for v in d.values()]
     
     return out
 
 
 
-class BeagleOrderSingle(model.Model):
+class BeagleOrderSeq(object):
 
-    def train(self,
-              corpus,
-              env_matrix=None,
-              psi=None,
-              rand_perm=None,
-              n_columns=2048,
-              lmda = 7,
-              tok_name='sentences'):
+    def __init__(self, corpus, env_matrix, tok_name='sentence',
+                 psi=None, rand_perm=None, lmda =7):
+        """
+        """
+        self.sents = corpus.view_tokens(tok_name)
+        self.env_matrix = env_matrix
+
+        self.b_conv = mk_b_conv(env_matrix.shape[1], rand_perm)
         
-        if env_matrix is None:
-            
-            m = be.BeagleEnvironment()
-
-            m.train(corpus, n_columns=n_columns)
-
-            env_matrix = m.matrix[:, :]
-
-        b_conv = mk_b_conv(n_columns, rand_perm)
-
         if psi is None:
-
-            psi = rand_pt_unit_sphere(env_matrix.shape[1])
-
-        self.matrix = np.zeros_like(env_matrix)
-
-
-
-        if isinstance(corpus, corp.MaskedCorpus):
-
-            sents = corpus.view_tokens(tok_name, unmask=True)
-
+            self.psi = rand_pt_unit_sphere(env_matrix.shape[1])
         else:
+            self.psi = psi
 
-            sents = corpus.view_tokens(tok_name)
+        self.lmda = lmda
 
-        for sent in sents:
 
-            for i in xrange(sent.shape[0]):
+    def train(self):
+        """
+        """
+        self.matrix = np.zeros_like(self.env_matrix)
 
-                if corpus.terms[sent[i]] is not np.ma.masked:
+        for sent in self.sents:
+            env = self.env_matrix[sent]
+            
+            for i in xrange(sent.size):
+                sent_vecs = env.copy() 
+                sent_vecs[i, :] = self.psi[:]
+
+                conv_ngrams = reduce_ngrams(self.b_conv, sent_vecs, self.lmda, i)
                     
-                    left = [env_matrix[term] for term in sent[:i]]
-
-                    right = [env_matrix[term] for term in sent[i+1:]]
-                    
-                    sent_vecs = np.array(left + [psi] + right)
-                    
-                    conv_ngrams = reduce_ngrams(b_conv, sent_vecs, lmda, i)
-                    
-                    ord_vec = np.sum(conv_ngrams, axis=0)
-
-                    self.matrix[sent[i], :] += ord_vec
+                ord_vec = np.sum(conv_ngrams, axis=0)
+                self.matrix[sent[i], :] += ord_vec
 
 
 
-class BeagleOrderMulti(model.Model):
+class BeagleOrderMulti(object):
 
-    def train(self,
-              corpus,
-              env_matrix=None,
-              psi=None,
-              rand_perm=None,
-              n_columns=2048,
-              lmda = 7,
-              tok_name='sentences',
-              n_processes=20):
+    def __init__(self, corpus, env_matrix, tok_name='sentence',
+                 psi=None, rand_perm=None, lmda =7):
+        """
+        """
+        self.sents = corpus.view_tokens(tok_name)
+        self.dtype = env_matrix.dtype
 
-        global _lmda
-
-        _lmda = lmda
-
-        del lmda
-
-
-
-        global _b_conv
-        
-        _b_conv = mk_b_conv(n_columns, rand_perm)
-
-        del rand_perm
-
-
-
-        if env_matrix is None:
-
-            m = be.BeagleEnvironment()
-
-            m.train(corpus, n_columns=n_columns)
-
-            env_matrix = m.matrix[:]
-
-            del m
-
-        global _shape
-
-        _shape = env_matrix.shape
-
-
-        
-        global _env_matrix
+        global _shape 
+        _shape = mp.Array('i', 2, lock=False)
+        _shape[:] = env_matrix.shape
 
         print 'Copying env matrix to shared mp array'
-
-        _env_matrix = mp.Array('f', env_matrix.size, lock=False)
-
+        global _env_matrix
+        _env_matrix = mp.Array('d', env_matrix.size, lock=False)
         _env_matrix[:] = env_matrix.ravel()[:]
 
-        del env_matrix
+        #TODO: Convert this to a shared data structure
+        global _b_conv 
+        _b_conv = mk_b_conv(env_matrix.shape[1], rand_perm)
 
-
-
-        global _psi
-
-        _psi = mp.Array('f', _shape[1], lock=False)
-
+        global _psi        
+        _psi = mp.Array('d', _shape[1], lock=False)
         if psi is None:
-            
-            _psi[:] = rand_pt_unit_sphere(_shape[1])[:]
-
+            _psi[:] = rand_pt_unit_sphere(env_matrix.shape[1])[:]
         else:
-
             _psi[:] = psi[:]
 
-        del psi
+        global _lmda
+        _lmda = mp.Value('i', lock=False)
+        _lmda =lmda
 
 
+    def train(self, n_procs=2):
+                
+        sent_lists = np.array_split(self.sents, n_procs-1)
+        if len(sent_lists) != n_procs:
+            sent_lists = np.array_split(self.sents, n_procs)
 
-        print 'Gathering tokens over which to map'
-
-        if isinstance(corpus, corp.MaskedCorpus):
-
-            sents = corpus.view_tokens(tok_name, unmask=True)
-
-        else:
-
-            sents = corpus.view_tokens(tok_name)
-
-        k = len(sents) / (n_processes - 1)
-        
-        sent_lists = [sents[i * k:(i + 1) * k]
-                      for i in xrange(n_processes - 1)]
-        
-        sent_lists.append(sents[(i + 1) * k:])
-        
         tmp_dir = tempfile.mkdtemp()
-        
         tmp_files = [os.path.join(tmp_dir, 'tmp_' + str(i))
                      for i in xrange(len(sent_lists))]
 
-        sent_lists = [(sent_lists[i], tmp_files[i])
-                      for i in xrange(len(sent_lists))]
-
-        del sents
+        sent_lists = zip(sent_lists, tmp_files)
+        del self.sents
 
 
-
-        global _terms
-
-        _terms = corpus.terms
-
-        del corpus
-
-
-        
         try:
-
+            print 'Forking'
             # For debugging
             # tmp_files = map(mpfn, sent_lists)
             
-            print 'Forking'
-            
-            p = mp.Pool()
-            
+            p = mp.Pool(n_procs)
             tmp_files = p.map(mpfn, sent_lists, 1)
-            
             p.close()
-            
+
             print 'Reducing'
-            
-            self.matrix = np.zeros(_shape)
-            
+            self.matrix = np.zeros(tuple(_shape), dtype=self.dtype)
+
             for filename in tmp_files:
-                
-                result = np.memmap(filename, mode='r',
-                                   shape=_shape, dtype=np.float32)
 
-                self.matrix[:, :] += result[:, :]
+                with open(filename, 'rb') as f:
+                    result = cpickle.load(f)
 
-                self.matrix = np.float32(self.matrix)
-                
+                for k,v in result.iteritems():
+                    self.matrix[k, :] += v
+
         finally:
-
             print 'Removing', tmp_dir
-
             shutil.rmtree(tmp_dir)
-
+        
 
 
 def mpfn((sents, filename)):
-
-    n = _shape[1]
-
-    result = np.memmap(filename, mode='w+', shape=_shape, dtype=np.float32)
+    """
+    """
+    result = dict()
 
     for sent in sents:
 
-        for i,t in enumerate(sent):
+        env = np.empty((sent.size, _shape[1]), dtype=np.float64)
+        for i,w in enumerate(sent):
+            env[i, :] = _env_matrix[w*_shape[1]: (w+1)*_shape[1]]
 
-            if _terms[sent[i]] is not np.ma.masked:
+        for i,w in enumerate(sent):
+            sent_vecs = env.copy()
+            sent_vecs[i, :] = _psi[:]
 
-                left = [np.asarray(_env_matrix[term * n:(term + 1) * n])
-                        for term in sent[:i]]
-        
-                right = [np.asarray(_env_matrix[term * n:(term + 1) * n])
-                         for term in sent[i + 1:]]
-        
-                sent_vecs = np.array(left + [_psi] + right)
-        
-                conv_ngrams = reduce_ngrams(_b_conv, sent_vecs, _lmda, i)
+            conv_ngrams = reduce_ngrams(_b_conv, sent_vecs, _lmda, i)
+
+            ord_vec = np.sum(conv_ngrams, axis=0)
+
+            if w in result:
+                result[w] += ord_vec
+            else:
+                result[w] = ord_vec
             
-                ord_vec = np.sum(conv_ngrams, axis=0)
+    with open(filename, 'wb') as f:
+        cpickle.dump(result, f)
 
-                result[t] += ord_vec
-
-    del result
-    
     return filename
-
-
-
-class BeagleOrder(BeagleOrderMulti):
-
-    pass
-
 
 
 #
@@ -362,44 +249,35 @@ class BeagleOrder(BeagleOrderMulti):
 #
 
 
+def test_BeagleOrderSeq():
 
-def test_BeagleOrderSingle():
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    from vsm import corpus
+    c = random_corpus(1000, 50, 0, 20, tok_name='sentence')
 
-    n = 5
+    e = BeagleEnvironment(c, n_cols=100)
+    e.train()
 
-    c = corpus.random_corpus(1e2, 10, 1, 10, tok_name='sentences')
+    m = BeagleOrderSeq(c, e.matrix)
+    m.train()
 
-    c = c.to_maskedcorpus()
-
-    c.mask_terms(['0'])
-    
-    m = BeagleOrderSingle()
-
-    m.train(c, n_columns=n)
-
-    return c, m.matrix
+    return m.matrix
 
 
 
 def test_BeagleOrderMulti():
 
-    from vsm import corpus
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    n = 5
+    c = random_corpus(1000, 50, 0, 20, tok_name='sentence')
 
-    print 'Generating corpus'
-    
-    c = corpus.random_corpus(1e2, 10, 1, 10, tok_name='sentences')
+    e = BeagleEnvironment(c, n_cols=100)
+    e.train()
 
-    c = c.to_maskedcorpus()
-
-    c.mask_terms(['0'])
-
-    m = BeagleOrderMulti()
-
-    m.train(c, n_columns=n)
+    m = BeagleOrderMulti(c, e.matrix)
+    m.train(4)
 
     return m.matrix
 
@@ -407,130 +285,87 @@ def test_BeagleOrderMulti():
 
 def test_compare():
 
-    from vsm import corpus
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    n = 4
+    c = random_corpus(1000, 100, 0, 20, tok_name='sentence')
 
-    c = corpus.random_corpus(1e3, 20, 1, 10, tok_name='sentences')
+    e = BeagleEnvironment(c, n_cols=5)
+    e.train()
 
-    c = c.to_maskedcorpus()
+    psi = rand_pt_unit_sphere(e.shape[1])
 
-    c.mask_terms(['0'])
-
-    em = be.BeagleEnvironment()
-
-    em.train(c, n_columns=n)
-
-    env_matrix = em.matrix
-
-    psi = rand_pt_unit_sphere(n)
-
-    rand_perm = two_rand_perm(n)
+    rand_perm = two_rand_perm(e.shape[1])
 
     print 'Training single processor model'
-
-    sm = BeagleOrderSingle()
-
-    sm.train(c, psi=psi, env_matrix=env_matrix, rand_perm=rand_perm)
+    ms = BeagleOrderSeq(c, e.matrix, psi=psi, rand_perm=rand_perm)
+    ms.train()
 
     print 'Training multiprocessor model'
+    mm = BeagleOrderMulti(c, e.matrix, psi=psi, rand_perm=rand_perm)
+    mm.train()
 
-    mm = BeagleOrderMulti()
-
-    mm.train(c, psi=psi, env_matrix=env_matrix, rand_perm=rand_perm)
-
-    assert np.allclose(sm.matrix, mm.matrix, atol=1e-07)
-
+    assert np.allclose(ms.matrix, mm.matrix), (ms.matrix, mm.matrix)
 
 
 
 def test10():
 
-    a = np.arange(5)
-
-    def fn(x,y):
-
-        if isinstance(x, tuple):
-
-            return x + (y,)
-
-        return (x, y)
-
     import pprint
 
+    def fn(x,y):
+        if isinstance(x, tuple):
+            return x + (y,)
+        return (x, y)
+
+    a = np.arange(5)
     print 'array length', a.shape[0]
         
     for i in xrange(a.shape[0]):
-
         n = 3
-
         print 'ngram length', n
-
         print 'index', i
-
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
     for i in xrange(a.shape[0]):
-
         n = 4
-
         print 'ngram length', n
-
         print 'index', i
-
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
     for i in xrange(a.shape[0]):
-                
         n = 5
-            
         print 'ngram length', n
-
         print 'index', i
-        
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
 
 
 def test11():
 
-    a = np.arange(5)
-
-    def fn(x,y):
-
-        return x + y
-
     import pprint
 
+    def fn(x,y):
+        return x + y
+
+    a = np.arange(5)
     print 'array length', a.shape[0]
         
     for i in xrange(a.shape[0]):
-
         n = 3
-
         print 'ngram length', n
-
         print 'index', i
-
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
     for i in xrange(a.shape[0]):
-
         n = 4
-
         print 'ngram length', n
-
         print 'index', i
-
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
     for i in xrange(a.shape[0]):
-                
         n = 5
-            
         print 'ngram length', n
-
         print 'index', i
-        
         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
