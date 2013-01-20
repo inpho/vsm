48a1327e42d2543fe854c45f16b0da15b0ee411e
Robert Rose <rrose1@indiana.edu>
2013-01-20 15:03:34 -0500

    beaglecontext.py updated.

--- vsm/model/beaglecontext.py
+++ vsm/model/beaglecontext.py
@@ -2,231 +2,142 @@ import os
 import shutil
 import tempfile
 import multiprocessing as mp
+import cPickle as cpickle
 
 import numpy as np
 
-from vsm import corpus as corp
-from vsm import model
-from vsm.model import beagleenvironment as be
 
 
+class BeagleContextSeq(object):
 
-class BeagleContextSingle(model.Model):
-
-    def train(self,
-              corpus,
-              env_matrix=None,
-              n_columns=2048,
-              tok_name='sentences'):
-
-        if env_matrix is None:
-
-            m = be.BeagleEnvironment()
-
-            m.train(corpus, n_columns=n_columns)
-
-            env_matrix = m.matrix[:, :]
-
-        self.matrix = np.zeros_like(env_matrix)
+    def __init__(self, corpus, env_matrix, tok_name='sentence'):
+        """
+        """
+        self.sents = corpus.view_tokens(tok_name)
+        self.env_matrix = env_matrix
 
 
+    def train(self):
+        """
+        """
+        self.matrix = np.zeros_like(self.env_matrix)
 
-        if isinstance(corpus, corp.MaskedCorpus):
-
-            sents = corpus.view_tokens(tok_name, compress=True)
-
-        else:
-
-            sents = corpus.view_tokens(tok_name)
-
-
-
-        for sent in sents:
+        for sent in self.sents:
 
             if sent.shape[0] > 1:
 
-                left_sums = np.cumsum(env_matrix[sent[:-1]], axis=0)
-
-                right_sums = np.cumsum(env_matrix[sent[:0:-1]], axis=0)
+                left_sums = np.cumsum(self.env_matrix[sent[:-1]], axis=0)
+                right_sums = np.cumsum(self.env_matrix[sent[:0:-1]], axis=0)
 
                 for i,term in enumerate(sent):
 
                     if i == 0:
-
                         ctx_vector = right_sums[-1]
 
                     elif i == sent.shape[0] - 1:
-                        
                         ctx_vector = left_sums[-1]
                     
                     else:
-
                         ctx_vector = left_sums[i - 1] + right_sums[-i - 1]
 
                     self.matrix[term, :] += ctx_vector
 
 
 
-class BeagleContextMulti(model.Model):
+class BeagleContextMulti(object):
 
-    def train(self,
-              corpus,
-              env_matrix=None,
-              n_columns=2048,
-              tok_name='sentences',
-              n_processes=20):
-
-        if env_matrix is None:
-
-            m = be.BeagleEnvironment()
-
-            m.train(corpus, n_columns=n_columns)
-
-            env_matrix = m.matrix[:]
-
-            del m
-
-        global _shape
-
-        _shape = env_matrix.shape
-
-
+    def __init__(self, corpus, env_matrix, tok_name='sentence'):
+        """
+        """
+        self.sents = corpus.view_tokens(tok_name)
+        self.dtype = env_matrix.dtype
         
-        global _env_matrix
+        global _shape 
+        _shape = mp.Array('i', 2, lock=False)
+        _shape[:] = env_matrix.shape
 
         print 'Copying env matrix to shared mp array'
-
-        _env_matrix = mp.Array('f', env_matrix.size, lock=False)
-
+        global _env_matrix
+        _env_matrix = mp.Array('d', env_matrix.size, lock=False)
         _env_matrix[:] = env_matrix.ravel()[:]
 
-        del env_matrix
 
+    def train(self, n_procs=2):
+        """
+        """
+        sent_lists = np.array_split(self.sents, n_procs-1)
+        if len(sent_lists) != n_procs:
+            sent_lists = np.array_split(self.sents, n_procs)
 
-
-        print 'Gathering tokens over which to map'
-
-        if isinstance(corpus, corp.MaskedCorpus):
-
-            sents = corpus.view_tokens(tok_name, compress=True)
-
-        else:
-
-            sents = corpus.view_tokens(tok_name)
-
-        k = len(sents) / (n_processes - 1)
-        
-        sent_lists = [sents[i * k:(i + 1) * k]
-                      for i in xrange(n_processes - 1)]
-        
-        sent_lists.append(sents[(i + 1) * k:])
-        
         tmp_dir = tempfile.mkdtemp()
-        
         tmp_files = [os.path.join(tmp_dir, 'tmp_' + str(i))
                      for i in xrange(len(sent_lists))]
 
-        sent_lists = [(sent_lists[i], tmp_files[i])
-                      for i in xrange(len(sent_lists))]
-
-        del sents
-
-        del corpus
-
-
+        sent_lists = zip(sent_lists, tmp_files)
+        del self.sents
 
         try:
-
+            print 'Forking'
             # For debugging
             # tmp_files = map(mpfn, sent_lists)
-
-            print 'Forking'
             
-            p = mp.Pool()
-            
+            p = mp.Pool(n_procs)
             tmp_files = p.map(mpfn, sent_lists, 1)
-            
             p.close()
 
             print 'Reducing'
-            
-            self.matrix = np.zeros(_shape)
+            self.matrix = np.zeros(tuple(_shape), dtype=self.dtype)
 
             for filename in tmp_files:
 
-                result = np.memmap(filename, mode='r',
-                                   shape=_shape, dtype=np.float32)
+                with open(filename, 'rb') as f:
+                    result = cpickle.load(f)
 
-                self.matrix[:, :] += result[:, :]
-
-                self.matrix = np.float32(self.matrix)
+                for k,v in result.iteritems():
+                    self.matrix[k, :] += v
 
         finally:
-
             print 'Removing', tmp_dir
-
             shutil.rmtree(tmp_dir)
 
 
 
 def mpfn((sents, filename)):
-
-    n = _shape[1]
-
-    result = np.memmap(filename, mode='w+', shape=_shape, dtype=np.float32)
+    """
+    """
+    result = dict()
 
     for sent in sents:
-        
         if sent.shape[0] > 1:
 
-            lslice = slice(sent[0] * n, (sent[0] + 1) * n)
-
-            left_sums = [np.array(_env_matrix[lslice])]
-
-            rslice = slice(sent[-1] * n, (sent[-1] + 1) * n)
-
-            right_sums = [np.array(_env_matrix[rslice])]
-
-            for i in xrange(1, sent.shape[0] - 1):
-
-                lslice = slice(sent[i] * n, (sent[i] + 1) * n)
-
-                next_left = np.array(_env_matrix[lslice])
-
-                left_sums.append(left_sums[-1] + next_left)
-
-                rslice = slice(sent[-i - 1] * n, (sent[-i - 1] + 1) * n)
-
-                next_right = np.array(_env_matrix[rslice])
-
-                right_sums.append(right_sums[-1] + next_right)
+            env = np.empty((sent.size, _shape[1]), dtype=np.float64)
+            for i,w in enumerate(sent):
+                env[i, :] = _env_matrix[w*_shape[1]: (w+1)*_shape[1]]
+        
+            left_sums = np.cumsum(env[:-1], axis=0)
+            right_sums = np.cumsum(env[:0:-1], axis=0)
 
             for i,t in enumerate(sent):
                 
                 if i == 0:
-
                     ctx_vector = right_sums[-1]
 
                 elif i == sent.shape[0] - 1:
-                        
                     ctx_vector = left_sums[-1]
                     
                 else:
-
                     ctx_vector = left_sums[i - 1] + right_sums[-i - 1]
-                    
-                result[t, :] += ctx_vector
+                   
+                if t in result:
+                    result[t] += ctx_vector
+                else:
+                    result[t] = ctx_vector
 
-    del result
-    
+    with open(filename, 'wb') as f:
+        cpickle.dump(result, f)
+
     return filename
 
-
-
-
-class BeagleContext(BeagleContextMulti):
-
-    pass
 
 
 
@@ -236,72 +147,54 @@ class BeagleContext(BeagleContextMulti):
 
 
 
-def test_BeagleContextSingle():
+def test_BeagleContextSeq():
 
-    from vsm import corpus
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    n = 5
+    c = random_corpus(100000, 5000, 0, 20, tok_name='sentence')
 
-    c = corpus.random_corpus(1e5, 1e4, 1, 20, tok_name='sentences')
+    e = BeagleEnvironment(c, n_cols=200)
+    e.train()
 
-    c = c.to_maskedcorpus()
-
-    c.mask_terms(['0'])
-        
-    m = BeagleContextSingle()
-
-    m.train(c, n_columns=n)
+    m = BeagleContextSeq(c, e.matrix)
+    m.train()
 
     return m.matrix
-
 
 
 def test_BeagleContextMulti():
 
-    from vsm import corpus
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    n = 2048
+    c = random_corpus(100000, 5000, 0, 20, tok_name='sentence')
 
-    print 'Generating corpus'
-    
-    c = corpus.random_corpus(1e6, 1e5, 1, 20, tok_name='sentences')
+    e = BeagleEnvironment(c, n_cols=200)
+    e.train()
 
-    c = c.to_maskedcorpus()
-
-    c.mask_terms(['0'])
-    
-    m = BeagleContextMulti()
-
-    m.train(c, n_columns=n)
+    m = BeagleContextMulti(c, e.matrix)
+    m.train(n_procs=3)
 
     return m.matrix
 
 
-
 def test_compare():
 
-    from vsm import corpus
+    from vsm.util.corpustools import random_corpus
+    from vsm.model.beagleenvironment import BeagleEnvironment
 
-    n = 4
+    c = random_corpus(10000, 1000, 0, 20, tok_name='sentence')
 
-    c = corpus.random_corpus(1e3, 20, 1, 10, tok_name='sentences')
-
-    em = be.BeagleEnvironment()
-
-    em.train(c, n_columns=n)
-
-    env_matrix = em.matrix
+    e = BeagleEnvironment(c, n_cols=5)
+    e.train()
 
     print 'Training single processor model'
-
-    sm = BeagleContextSingle()
-
-    sm.train(c, env_matrix=env_matrix)
+    ms = BeagleContextSeq(c, e.matrix)
+    ms.train()
 
     print 'Training multiprocessor model'
+    mm = BeagleContextMulti(c, e.matrix)
+    mm.train()
 
-    mm = BeagleContextMulti()
-
-    mm.train(c, env_matrix=env_matrix)
-
-    assert np.allclose(sm.matrix, mm.matrix, atol=1e-07), (sm.matrix[:2], mm.matrix[:2])
+    assert np.allclose(ms.matrix, mm.matrix)
