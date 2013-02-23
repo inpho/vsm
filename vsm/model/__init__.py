a78ad2ba9fdacffaeb92acab3220f19932ddb6ec
Robert Rose <rrose1@indiana.edu>
2013-02-23 16:31:25 -0500

    Updated documentation in vsm.model

--- vsm/model/__init__.py
+++ vsm/model/__init__.py
@@ -4,10 +4,36 @@ import numpy as np
 
 class BaseModel(object):
     """
+    Base class for models which store data in a single matrix.
+
+    Parameters
+    ----------
+    matrix : numpy.ndarray
+        A two-dimensional numpy array storing the results of model
+        training. Default is `None`.
+    context_type : string
+        A string specifying the type of context over which the model
+        trainer is applied. Default is `None`.
+
+    Attributes
+    ----------
+    Same as parameters.
+
+    Methods
+    -------
+    save
+        Takes a filename or file object and saves `self.matrix` in an
+        npz archive.
+    load
+        Takes a filename or file object and loads it as an npz archive
+        into a BaseModel object.
+
+    See Also
+    --------
+    numpy.savez
+    numpy.load
     """
     def __init__(self, matrix=None, context_type=None):
-        """
-        """
         self.matrix = matrix
         self.context_type = context_type
 
