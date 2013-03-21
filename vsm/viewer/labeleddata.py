4aefd8398a030231f404c77a3c839deb6158e872
doori <dl28@umail.iu.edu>
2013-03-21 17:24:09 -0400

    Bug fixed in slicing DataTable.

--- vsm/viewer/labeleddata.py
+++ vsm/viewer/labeleddata.py
@@ -333,8 +333,11 @@ class DataTable(list):
         super(DataTable, self).__init__(l)
         self.table_header = table_header
 
+
     def __getslice__(self, i, j):
-	return DataTable(list.__getslice__(self, i, j))
+	return DataTable(list.__getslice__(self, i, j), 
+					table_header=self.table_header)
+
 
     def __str__(self):
         """
