76e0f1c5bae7d17cca5528786fd59a0b4ee4acc9
doori <dl28@umail.iu.edu>
2013-03-24 10:34:58 -0400

    Fixed bug in remove_empty.

--- vsm/corpus/__init__.py
+++ vsm/corpus/__init__.py
@@ -202,7 +202,7 @@ class BaseCorpus(object):
 	for j, t in enumerate(self.context_types):
 	    token_list = self.view_contexts(t)
 
-	    indices = [ctx.size != 0 for ctx in token_list]
+	    indices = np.array([ctx.size != 0 for ctx in token_list], dtype=np.bool)
 	    self.context_data[j] = self.context_data[j][indices]
 
 
