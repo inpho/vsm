54fbc16bd9e2d23d079261d3aaa8dc05830b0116
Tim Downey <tcdowney@indiana.edu>
2013-03-24 14:51:39 -0400

    Added test_BeagleOrderMulti

--- tests/model/beagleorder_test.py
+++ tests/model/beagleorder_test.py
@@ -25,6 +25,25 @@ class TestBeagleOrderModel(unittest.TestCase):
             os.remove(tmp.name)
         self.assertTrue(modelsEqual, msg=None)
 
+    def test_BeagleOrderMulti(self):
+        corp = random_corpus(1000, 50, 0, 20, context_type='sentence')
+        env = BeagleEnvironment(corp, n_cols=100)
+        env.train()
+
+        model = BeagleOrderMulti(corp, env.matrix)
+        model.train(4)
+
+        try:
+            tmp = NamedTemporaryFile(delete=False, suffix='.npz')
+            model.save(tmp.name)
+            tmp.close()
+            loadedModel = BeagleOrderSeq.load(tmp.name)
+
+            modelsEqual = (model.matrix == loadedModel.matrix).all()
+        finally:
+            os.remove(tmp.name)
+        self.assertTrue(modelsEqual, msg=None)
+
 
 if __name__ == '__main__':
     unittest.main()
