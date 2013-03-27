824cabf3d70d66dfbb29e99cafedbca9b07e5bfe
Rpenelton <ryanpene@indiana.edu>
2013-03-27 16:41:43 -0300

    Update tf.py

--- vsm/model/tf.py
+++ vsm/model/tf.py
@@ -77,16 +77,42 @@ class TfModel(BaseModel):
                                         dtype=np.int32)
 
 
-def combine_models(model_1, model_2):
+def combine_models(m1, m2):
     """Takes two models. Chooses which model is bigger and clones it to a third
     model. It then iterates over the second model adding values that already
     exists into the third model and creating new entries for those that
     didn't already exist"""
-    size_1 = sparse.model_1.get_shape()
-    size_2 = sparse.model_2.get_shape()
-    model_3 = model_1
-    for i,j,v in zip(model_2.row, model_2.col, model_3.data):
-        print "row = %d, column = %d, value = %s" % (i,j,v)
+    size_1 = m1.get_shape()
+    size_2 = m2.get_shape()
+    data_1 = m1.data
+    data_2 = m2.data
+    size_of_data3 = 0
+
+    if np.size(data_1) >= np.size(data_2):
+        size_of_data3 = np.size(data_1)
+        while np.size(data_1) != np.size(data_2):
+            np.append(data_2, 0)
+    else:
+        size_of_data3 = np.size(data_2)
+        while np.size(data_2) != np.size(data_1):
+            np.append(data_1, 0)
+        
+    words_in_data1 = 0
+    data_3 = np.concatenate(data_1, data_2)
+    data_3 = np.trim_zeros(data_3)
+    rows = np.arrange(np.size(data_3))
+    cols = np.arrange(np.size(data_3))
+    coo_in = (data_3, (rows, cols))
+    model_3 = sparse.coo_matrix(coo_in, shape=((np.size(data_3)), (np.size(data_3))))
+    return model_3
+
+
+def combine_corpus(c1, c2):
+    """Takes two corpora and combines them into one super corpus. Also keeps an association list between the two old corpora and the new corpus."""
+    
+    alist_c1 = []
+    alist_c2 = []
+    c3 = [c1.words[i] for i in c1.corpus]
 
 
 
