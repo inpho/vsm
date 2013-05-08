e7b741b8e47584bfbaf483930d1a9e112f3d6a4c
Rpenelton <ryanpene@indiana.edu>
2013-05-08 16:02:42 -0400

    Update tf.py

--- vsm/model/tf.py
+++ vsm/model/tf.py
@@ -77,49 +77,50 @@ class TfModel(BaseModel):
                                         dtype=np.int32)
 
 
-def combine_models(m1, m2):
-    """Takes two models. Chooses which model is bigger and clones it to a third
-    model. It then iterates over the second model adding values that already
-    exists into the third model and creating new entries for those that
-    didn't already exist"""
-    size_1 = m1.get_shape()
-    size_2 = m2.get_shape()
-    data_1 = m1.data
-    data_2 = m2.data
-    size_of_data3 = 0
-
-    if np.size(data_1) >= np.size(data_2):
-        size_of_data3 = np.size(data_1)
-        while np.size(data_1) != np.size(data_2):
-            np.append(data_2, 0)
-    else:
-        size_of_data3 = np.size(data_2)
-        while np.size(data_2) != np.size(data_1):
-            np.append(data_1, 0)
-        
-    words_in_data1 = 0
-    data_3 = np.concatenate(data_1, data_2)
-    data_3 = np.trim_zeros(data_3)
-    rows = np.arrange(np.size(data_3))
-    cols = np.arrange(np.size(data_3))
-    coo_in = (data_3, (rows, cols))
-    model_3 = sparse.coo_matrix(coo_in, shape=((np.size(data_3)), (np.size(data_3))))
-    return model_3
-
-
-def combine_corpus(c1, c2):
-    """Takes two corpora and combines them into one super corpus. Also keeps an association list between the two old corpora and the new corpus."""
+    def combine_models(m1, m2):
+        """Takes two models. Chooses which model is bigger and clones it to a third
+        model. It then iterates over the second model adding values that already
+        exists into the third model and creating new entries for those that
+        didn't already exist"""
+        size_1 = m1.get_shape()
+        size_2 = m2.get_shape()
+        data_1 = m1.data
+        data_2 = m2.data
+        size_of_data3 = 0
     
-    from vsm.corpus import Corpus
-    c1dict = {}
-    c2dict = {}
-    text = [c1.words[i] for i in c1.corpus] + [c2.words[i] for i in c2.corpus]
-    c3 = Corpus(text)
-    for i,word in enumerate(c3.words):
-        if word in c1.words:
-            c1dict[word] = i
-        if word in c2.words:
-            c2dict[word] = i
+        if np.size(data_1) >= np.size(data_2):
+            size_of_data3 = np.size(data_1)
+            while np.size(data_1) != np.size(data_2):
+                np.append(data_2, 0)
+        else:
+            size_of_data3 = np.size(data_2)
+            while np.size(data_2) != np.size(data_1):
+                np.append(data_1, 0)
+            
+        words_in_data1 = 0
+        data_3 = np.concatenate(data_1, data_2)
+        data_3 = np.trim_zeros(data_3)
+        rows = np.arrange(np.size(data_3))
+        cols = np.arrange(np.size(data_3))
+        coo_in = (data_3, (rows, cols))
+        self.model_3 = sparse.coo_matrix(coo_in, shape=((np.size(data_3)), (np.size(data_3))))
+        return model_3
+    
+    
+    def combine_corpus(c1, c2):
+        """Takes two corpora and combines them into one super corpus. Also keeps an association list
+        between the two old corpora and the new corpus."""
+        
+        from vsm.corpus import Corpus
+        self.c1dict = {}
+        self.c2dict = {}
+        text = [c1.words[i] for i in c1.corpus] + [c2.words[i] for i in c2.corpus]
+        self.c3 = Corpus(text)
+        for i,word in enumerate(c3.words):
+            if word in c1.words:
+                self.c1dict[word] = i
+            if word in c2.words:
+                self.c2dict[word] = i
 
 def test_TfModel():
 
