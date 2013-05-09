a69fe2c37608bfc74c6afb746a18bfe1b1faf12b
Jesse Squires <jesse.d.squires@gmail.com>
2013-05-09 13:01:49 -0400

    removed php

--- tutorials/lda1/index.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<?php $thisPage="tutorial"; ?>
-<?php include('includes/header.php'); ?>
-
-				<h1>LDA (1): Training a model from a single file</h1>
-				<p>Contents</p>
-
-<?php include('includes/footer.php'); ?>
