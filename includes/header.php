c4fc767e9f6a74260af6a6b370971d3a86832bd4
Jesse Squires <jesse.d.squires@gmail.com>
2013-05-09 12:47:28 -0400

    site structure

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,11 @@
-<!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="chrome=1">
-		<title>vsm by inpho</title>
+		<title>VSM by InPhO</title>
 
-		<link rel="stylesheet" href="../../stylesheets/styles.css">
-		<link rel="stylesheet" href="../../stylesheets/pygment_trac.css">
+		<link rel="stylesheet" href="<?php if($thisPage != 'home') echo '../../' ?>stylesheets/styles.css">
+		<link rel="stylesheet" href="<?php if($thisPage != 'home') echo '../../' ?>stylesheets/pygment_trac.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<!--[if lt IE 9]>
 		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -15,7 +14,7 @@
 	<body>
 		<div class="wrapper">
 			<header>
-				<h1>VSM</h1>
+				<h1><a href="/">VSM</a></h1>
 				<p>The Vector Space Model Framework developed for the InPhO Project.</p>
 				<ul>
 					<li><a href="https://github.com/inpho/vsm/zipball/master">Download <strong>ZIP File</strong></a></li>
@@ -24,14 +23,3 @@
 				</ul>
 			</header>
 			<section>
-				<h1>LDA (1): Training a model from a single file</h1>
-				<p>Contents</p>
-			</section>
-			<footer>
-				<p>This project is maintained by <a href="https://github.com/inpho">InPhO</a></p>
-				<p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
-			</footer>
-		</div>
-		<script src="../../javascripts/scale.fix.js"></script>
-	</body>
-</html>
