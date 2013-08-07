f60da16cbe9f8bbaa75af5aea99c6c586aad8ac0
Jun Otsuka <junotk@Juns-MacBook-Air.local>
2013-08-07 16:47:42 -0700

    sim_top_doc in similarity.py now accept posterior calculation

--- vsm/viewer/ldagibbsviewer.py
+++ vsm/viewer/ldagibbsviewer.py
@@ -364,21 +364,15 @@ class LDAGibbsViewer(object):
 
     def sim_top_doc(self, topic_or_topics, weights=[], filter_words=[],
                     print_len=10, as_strings=True, label_fn=_def_label_fn_, 
-                    filter_nan=True):
+                    filter_nan=True, method='posterior'):
         """
         """
-        dt  = self.model.doc_top
-        out   = np.zeros(dt.shape[0])
-        norms = dt.sum(axis=1)
-        for (k, w) in zip(topic_or_topics, weights):
-            pr_kD = dt[:, k] / norms
-            out += w * pr_kD / pr_kD.sum()
-
-#        d_arr = _sim_top_doc_(self.corpus, self.model.doc_top, topic_or_topics, 
-#                              self.model.context_type, weights=weights, 
-#                              norms=self._doc_norms, print_len=print_len,
-#                              as_strings=False, label_fn=label_fn, 
-#                              filter_nan=filter_nan)
+        d_arr = _sim_top_doc_(self.corpus, self.model.doc_top, topic_or_topics, 
+                              self.model.context_type, weights=weights, 
+                              norms=self._doc_norms, print_len=print_len,
+                              as_strings=False, label_fn=label_fn, 
+                              filter_nan=filter_nan,
+                              method=method)
         
         topics = _res_top_type_(topic_or_topics)
 
--- vsm/viewer/similarity.py
+++ vsm/viewer/similarity.py
@@ -137,6 +137,7 @@ def sim_word_top(corp, mat, word_or_words, weights=[], norms=None,
 def sim_top_doc(corp, mat, topic_or_topics, context_type, weights=[], 
                 norms=None, print_len=10, filter_nan=True, 
                 label_fn=def_label_fn, as_strings=True,
+                method='posterior', 
                 sim_fn=row_cosines, order='d'):
     """
     Takes a topic or list of topics (by integer index) and returns a
@@ -144,18 +145,28 @@ def sim_top_doc(corp, mat, topic_or_topics, context_type, weights=[],
     documents given the topic.
     """
     topics = res_top_type(topic_or_topics)
-            
     # Assume documents are rows
-
-    # Generate pseudo-document
-    doc = np.zeros((1, mat.shape[1]), dtype=np.float64)
-    if len(weights) == 0:
-        doc[:, topics] = np.ones(len(topics))
+            
+    if method == 'posterior':
+        if len(weights) == 0:
+            weights = np.ones(len(topics)) / len(topics)   # equal weights
+        d_arr   = np.zeros(mat.shape[0])
+        norms = mat.sum(axis=1)
+        for (k, w) in zip(topics, weights):
+            post_k = mat[:, k] / norms      # posteriors of topic k given various values of D
+            post_d = post_k / post_k.sum()  # posteriors of D given topic k, assuming uniform prior for D
+            d_arr += w * post_d
+        d_arr /= d_arr.sum()
     else:
-        doc[:, topics] = weights
+        # Generate pseudo-document
+        doc = np.zeros((1, mat.shape[1]), dtype=np.float64)
+        if len(weights) == 0:
+            doc[:, topics] = np.ones(len(topics))
+        else:
+            doc[:, topics] = weights
 
-    # Compute similarites
-    d_arr = sim_fn(doc, mat, norms=norms)
+        # Compute similarites
+        d_arr = sim_fn(doc, mat, norms=norms)
 
     # Label data
     if as_strings:
