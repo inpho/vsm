69948fb3c05645d68873952083c65ea6d7bcb3fc
doori <dl28@umail.iu.edu>
2013-09-21 22:21:22 -0400

    Added unittests for core.

--- unit_tests/tests_corpus_util.py
+++ unit_tests/tests_corpus_util.py
@@ -1,6 +1,7 @@
 import unittest2 as unittest
 
 from vsm.corpus import util
+from vsm.corpus.util.corpusbuilders import *
 
 
 
@@ -18,7 +19,40 @@ class TestCorpusUtil(unittest.TestCase):
         self.assertTrue((c.context_data[0]['sent_label'] ==
                          ['sent_1', 'sent_3']).all())
 
+    def test_toy_corpus(self):
+        keats = ('She dwells with Beauty - Beauty that must die;\n\n'
+                 'And Joy, whose hand is ever at his lips\n\n'
+                 'Bidding adieu; and aching Pleasure nigh,\n\n'
+                 'Turning to poison while the bee-mouth sips:\n\n'
+                 'Ay, in the very temple of Delight\n\n'
+                 'Veil\'d Melancholy has her sovran shrine,\n\n'
+                 'Though seen of none save him whose strenuous tongue\n\n'
+                 'Can burst Joy\'s grape against his palate fine;\n\n'
+                 'His soul shall taste the sadness of her might,\n\n'
+                 'And be among her cloudy trophies hung.')
 
-#Define and run test suite
+        self.assertTrue(toy_corpus(keats))
+        self.assertTrue(toy_corpus(keats, nltk_stop=True))
+        self.assertTrue(toy_corpus(keats, stop_freq=1))
+        self.assertTrue(toy_corpus(keats, add_stop=['and', 'with']))
+        self.assertTrue(toy_corpus(keats, nltk_stop=True,
+                      stop_freq=1, add_stop=['ay']))
+
+        import os
+        from tempfile import NamedTemporaryFile as NFT
+
+        tmp = NFT(delete=False)
+        tmp.write(keats)
+        tmp.close()
+
+        c = toy_corpus(tmp.name, is_filename=True,
+                       nltk_stop=True, add_stop=['ay'])
+    
+        self.assertTrue(c)
+        os.remove(tmp.name)
+
+        return c
+
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestCorpusUtil)
 unittest.TextTestRunner(verbosity=2).run(suite)
