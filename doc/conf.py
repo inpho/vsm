7290065ed2e174d150c2284d35e8c51f830a209b
Robert Rose <rrose1@indiana.edu>
2013-10-05 15:58:39 -0400

    Removing ipython's sphinx code from vsm's repo.

--- doc/conf.py
+++ doc/conf.py
@@ -27,8 +27,9 @@ needs_sphinx = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 
-            'sphinx.ext.coverage', 'sphinx.ext.intersphinx', 'ipython_directive', 
-            'ipython_console_highlighting']#, 'matplotlib.sphinxext.ipython_directive']
+              'sphinx.ext.coverage', 'sphinx.ext.intersphinx',
+              # 'ipython_directive', 'ipython_console_highlighting'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
