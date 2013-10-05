7511c04517ec03aa356c82ec8ad70aea59fd68ca
doori <dl28@umail.iu.edu>
2013-10-05 17:48:23 -0400

    Moved beagle environment tests to unit_tests.

--- vsm/model/beagleenvironment.py
+++ vsm/model/beagleenvironment.py
@@ -23,33 +23,3 @@ class BeagleEnvironment(BaseModel):
         self.matrix = row_normalize(self.matrix)
 
 
-
-def test_BeagleEnvironment():
-
-    from vsm.util.corpustools import random_corpus
-    from vsm.linalg import row_norms
-
-    c = random_corpus(1000, 100, 0, 20)
-
-    m = BeagleEnvironment(c, n_cols=100)
-    m.train()
-
-    assert (m.matrix <= 1).all()
-    assert (m.matrix >= -1).all()
-
-    norms = row_norms(m.matrix)
-
-    assert np.allclose(np.ones(norms.shape[0]), norms)
-
-    from tempfile import NamedTemporaryFile
-    import os
-
-    try:
-        tmp = NamedTemporaryFile(delete=False, suffix='.npz')
-        m.save(tmp.name)
-        tmp.close()
-        m1 = BeagleEnvironment.load(tmp.name)
-        assert (m.matrix == m1.matrix).all()
-    
-    finally:
-        os.remove(tmp.name)
