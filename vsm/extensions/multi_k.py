bdfd8d448299eafa21195a07a366b7785b7b07a1
doori <dl28@umail.iu.edu>
2014-02-22 18:57:42 -0500

    Added refine() in progress.

--- vsm/extensions/multi_k.py
+++ vsm/extensions/multi_k.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import datasets
 from random import randrange
+from scipy.sparse import csgraph as cs
+
 
 # generate sample data
 n_samples = 30
@@ -12,7 +14,7 @@ blobs =  datasets.make_blobs(n_samples=n_samples, random_state=8)
 # S = noisy_circles[0]
 S = blobs[0]
 
-def multi_k(S, N=30, D_max=30):
+def multi_k(S, N=30, D_max=30, C=0.5):
     import matplotlib.pyplot as plt
 
     M = connection_matrix(S=S, N=N, D_max=D_max)
@@ -66,15 +68,69 @@ def connection_matrix(S, N=10, D_max=30):
 
 
 def cutplot(M, N=10):
-    from scipy.sparse import csgraph as cs
-
-    # build Weight matrix, W.
+    # Build Weight matrix, W. 
+    # Q. what's the purpose of the Weight matrix?
     W = M
     cutplot = []
-    for l in xrange(N):
+    for l in xrange(N+1):
         # Construct graph G for which M[i][j] > l/N
-        G = M > l * 1.0 /N
+        G = M > 1.0 * l /N
         
-        n_comp, label = cs.cs_graph_components(G)
+        n_comp, labels = cs.cs_graph_components(G)
         cutplot.append((l * 1.0 /N, n_comp))
-    return cutplot 
+    return cutplot
+
+
+def count_x(x, ls):
+    """
+    Counts the number of occurrences of `x` in list `ls`.
+    """
+    count = 0
+    for l in ls:
+        if l == x:
+            count += 1
+
+    return count
+
+def find_y(x, plot):
+    """
+    Given the x value, finds the y value.
+    plot is a list with tuples (x,y).
+    """
+    for i,j in plot:
+        if x == i:
+            return j
+    # if x is not found in plot
+    return 'val not found.'
+
+
+def refine(S, M, cutplot, N=10, C=0.5):
+    
+    cutplot_ = []
+    for i in xrange(len(cutplot)):
+        # !!! ERROR may be the wrong input, x.
+        # never hits the if < x < with these inputs.
+        x = cutplot[i][0]
+
+        for l in xrange(N + 1):
+            if 1.0 * l/N < x and x < 1.0 * (l + 1)/N:
+                # refine cut plot function.
+                it = 1.0 * l/N
+                y = find_y(it, cutplot)
+                cutplot_.append((x, y))
+        else:
+            cutplot_.append((x, cutplot[i][1]))
+
+    # Build a new graph on S with edges M[i][j] > C
+    newG = M > C
+    
+    categorize = []
+    n_comp, labels = cs.cs_graph_components(newG)
+    
+    for i in xrange(len(S)):
+        count = count_x(labels[i], labels)
+        categorize.append((S[i], count))
+
+    return categorize, cutplot_
+
+
