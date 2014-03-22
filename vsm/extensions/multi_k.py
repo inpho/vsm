2773461ac8f61b58d9bb6a58a0ee76e78fc766e6
doori <dl28@umail.iu.edu>
2014-03-22 00:47:45 -0400

    Changed variable names to lowercase.

--- vsm/extensions/multi_k.py
+++ vsm/extensions/multi_k.py
@@ -14,15 +14,15 @@ blobs =  datasets.make_blobs(n_samples=n_samples, random_state=8)
 # S = noisy_circles[0]
 S = blobs[0]
 
-def multi_k(S, N=30, D=(10,30), C=None):
+def multi_k(samples, n=30, distr=(10,30), cutoff=None):
     import matplotlib.pyplot as plt
 
-    M, cutplot = connection_matrix(S=S, N=N, D=D)
+    mat, cutplot = connection_matrix(samples=samples, n=n, distr=distr)
 
-    if C == None:
-        C = find_cutoff(cutplot)
-    print "Weight cutoff is set to ", C
-    category_func = category_mat(S, M, cutplot, C=C)
+    if cutoff == None:
+        cutoff = find_cutoff(cutplot)
+    print "Weight cutoff is set to ", cutoff
+    category_func = category_mat(samples, mat, cutplot, cutoff=cutoff)
 
     x, y = zip(*cutplot)
     plt.plot(x,y)
@@ -30,49 +30,49 @@ def multi_k(S, N=30, D=(10,30), C=None):
     return plt, cutplot, category_func
 
 
-def connection_matrix(S, N=10, D=(10,30)):
+def connection_matrix(samples, n=10, distr=(10,30)):
     """
-    S : set S of r points in Rn.
-    N : number N of clusterings to perform.
-    D : numbers of clusters.
-        <- this should be a distribution over 0,...,N. 
+    samples : set S of r points in Rn.
+    n : number n of clusterings to perform.
+    distr : numbers of clusters.
+        <- this should be a distribution over 0,...,n. 
         Let us use a uniform integer for a moment. 
-    C : weight cutoff, 0 <= C <= 1.
+    cutoff : weight cutoff, 0 <= cutoff <= 1.
     
-    could make N,D,C optional by setting default values.
+    could make n,distr,cutoff optional by setting default values.
     
-    :returns: M : connection matrix 
+    :returns: mat : connection matrix 
              cutplot :  A cut plot
                 f : [0,1] -> Z
     """
-    r = len(S)
-    M = np.zeros((r, r))
+    r = len(samples)
+    mat = np.zeros((r, r))
     
-    for n in xrange(N):
-        # select integer d from D[0] to D[1] 
-        d = randrange(D[0],D[1]+1)
+    for n_ in xrange(n):
+        # select integer d from distr[0] to distr[1] 
+        d = randrange(distr[0],distr[1]+1)
         km = KMeans(n_clusters=d, init='k-means++',
                     max_iter=100, n_init=1,verbose=False)
-        km.fit(S)
+        km.fit(samples)
         labels = km.labels_
 
         for i in range(r):
             for j in range(i,r):
                 if labels[i] == labels[j]:
-                    M[i][j] += 1
+                    mat[i][j] += 1
         
-    M = M + M.T
-    M /= N
+    mat = mat + mat.T
+    mat /= n
 
-    cutplot = np.zeros((N+1 ,2), dtype='f2<')
-    for l in xrange(N+1):
-        # Construct graph G for which M[i][j] > l/N
-        G = M > 1.0 * l /N
+    cutplot = np.zeros((n+1 ,2), dtype='f2<')
+    for l in xrange(n+1):
+        # Construct graph for which mat[i][j] > l/n
+        graph = mat > 1.0 * l /n
         
-        n_comp, labels = cs.cs_graph_components(G)
-        cutplot[l][0] = l * 1.0 /N
+        n_comp, labels = cs.cs_graph_components(graph)
+        cutplot[l][0] = l * 1.0 /n
         cutplot[l][1] = n_comp
-    return M, cutplot
+    return mat, cutplot
 
 
 def find_cutoff(cutplot):
@@ -89,14 +89,14 @@ def find_cutoff(cutplot):
             return c[0] + 0.1
 
 
-def category_mat(S, M, cutplot, C=None):
+def category_mat(samples, mat, cutplot, cutoff=None):
     """
     Predicts the category for each data point
     """ 
-    if C == None:
-        C = find_cutoff(cutplot)
-    # Build a new graph on S with edges M[i][j] > C
-    newG = M > C
+    if cutoff == None:
+        cutoff = find_cutoff(cutplot)
+    # Build a new graph on samples with edges mat[i][j] > cutoff
+    newG = mat > cutoff
     n_comp, labels = cs.cs_graph_components(newG)
     
     return labels
