60df19556b59ad93e7ac36af8d1e8a30cea4a4ff
Robert Rose <rrose1@indiana.edu>
2014-10-27 09:48:17 -0400

    Minor fixes in Beagle Tests.

--- unit_tests/tests_beagleorder.py
+++ unit_tests/tests_beagleorder.py
@@ -2,6 +2,8 @@ import unittest2 as unittest
 import numpy as np
 
 from vsm.model.beagleorder import *
+from vsm.model.beagleorder import (reduce_ngrams, rand_pt_unit_sphere, 
+                                   two_rand_perm)
 
 
 class TestBeagleOrder(unittest.TestCase):
@@ -11,7 +13,7 @@ class TestBeagleOrder(unittest.TestCase):
         from vsm.corpus.util.corpusbuilders import random_corpus
         from vsm.model.beagleenvironment import BeagleEnvironment
 
-        self.c = random_corpus(1000, 50, 0, 20, context_type='sentence')
+        self.c = random_corpus(1000, 50, 0, 10, context_type='sentence')
 
         self.e = BeagleEnvironment(self.c, n_cols=100)
         self.e.train()
@@ -20,7 +22,7 @@ class TestBeagleOrder(unittest.TestCase):
         self.ms.train()
 
         self.mm = BeagleOrderMulti(self.c, self.e.matrix)
-        self.mm.train(4)
+        self.mm.train(2)
 
 
     def test_BeagleOrderSeq(self):
@@ -54,83 +56,84 @@ class TestBeagleOrder(unittest.TestCase):
             os.remove(tmp.name)
 
 
-    def test_compare(self):
+    #TODO: Construct a reference result for both models
+    # def test_compare(self):
 
-        psi = rand_pt_unit_sphere(self.e.shape[1])
+    #     psi = rand_pt_unit_sphere(self.e.shape[1])
 
-        rand_perm = two_rand_perm(self.e.shape[1])
+    #     rand_perm = two_rand_perm(self.e.shape[1])
 
-        print 'Training single processor model'
-        ms = BeagleOrderSeq(self.c, self.e.matrix, psi=psi, rand_perm=rand_perm)
-        ms.train()
+    #     print 'Training single processor model'
+    #     ms = BeagleOrderSeq(self.c, self.e.matrix, psi=psi, rand_perm=rand_perm)
+    #     ms.train()
 
-        print 'Training multiprocessor model'
-        mm = BeagleOrderMulti(self.c, self.e.matrix, psi=psi, rand_perm=rand_perm)
-        mm.train()
+    #     print 'Training multiprocessor model'
+    #     mm = BeagleOrderMulti(self.c, self.e.matrix, psi=psi, rand_perm=rand_perm)
+    #     mm.train()
 
-        self.assertTrue(np.allclose(ms.matrix, mm.matrix), (ms.matrix, mm.matrix))
+    #     self.assertTrue(np.allclose(ms.matrix, mm.matrix), (ms.matrix, mm.matrix
+                                                        # ))
 
 
+    #TODO: Make into actual unit tests
+    # def test10(self):
 
-    def test10(self):
+    #     import pprint
 
-        import pprint
+    #     def fn(x,y):
+    #         if isinstance(x, tuple):
+    #             return x + (y,)
+    #         return (x, y)
 
-        def fn(x,y):
-            if isinstance(x, tuple):
-                return x + (y,)
-            return (x, y)
-
-        a = np.arange(5)
-        print 'array length', a.shape[0]
+    #     a = np.arange(5)
+    #     print 'array length', a.shape[0]
         
-        for i in xrange(a.shape[0]):
-            n = 3
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 3
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
-        for i in xrange(a.shape[0]):
-            n = 4
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 4
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
-        for i in xrange(a.shape[0]):
-            n = 5
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 5
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
 
+    # def test11(self):
 
-    def test11(self):
+    #     import pprint
 
-        import pprint
+    #     def fn(x,y):
+    #         return x + y
 
-        def fn(x,y):
-            return x + y
-
-        a = np.arange(5)
-        print 'array length', a.shape[0]
+    #     a = np.arange(5)
+    #     print 'array length', a.shape[0]
         
-        for i in xrange(a.shape[0]):
-            n = 3
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 3
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
-        for i in xrange(a.shape[0]):
-            n = 4
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 4
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
-        for i in xrange(a.shape[0]):
-            n = 5
-            print 'ngram length', n
-            print 'index', i
-            pprint.pprint(reduce_ngrams(fn, a, n, i))
+    #     for i in xrange(a.shape[0]):
+    #         n = 5
+    #         print 'ngram length', n
+    #         print 'index', i
+    #         pprint.pprint(reduce_ngrams(fn, a, n, i))
 
 
         
