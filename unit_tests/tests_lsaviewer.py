cf1b581f7db0fc38e35c80e2bd1229e44f9f4ba3
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-11-04 11:12:48 -0500

    fixing lsaviewer tests

--- unit_tests/tests_lsaviewer.py
+++ unit_tests/tests_lsaviewer.py
@@ -1,7 +1,7 @@
 import unittest2 as unittest
 import numpy as np
 
-from vsm.viewer.lsaviewer import *
+from vsm.viewer.lsaviewer import LsaViewer 
 
 
 class TestLsaViewer(unittest.TestCase):
@@ -29,19 +29,19 @@ class TestLsaViewer(unittest.TestCase):
 
     def test_Lsaviewer(self):
         
-        from vsm.viewer.labeleddata import *
+        from vsm.viewer.labeleddata import LabeledColumn, IndexedSymmArray 
 
-        sww = self.v.sim_word_word('1')
-        swwl = self.v.sim_word_word(['1', '0'])
-        sdd = self.v.sim_doc_doc(1)
-        sddl = self.v.sim_doc_doc([1, 0])
+        sww = self.v.dist_word_word('1')
+        swwl = self.v.dist_word_word(['1', '0'])
+        sdd = self.v.dist_doc_doc(1)
+        sddl = self.v.dist_doc_doc([1, 0])
         self.assertTrue(type(sww) == LabeledColumn)
         self.assertTrue(type(swwl) == LabeledColumn)
         self.assertTrue(type(sdd) == LabeledColumn)
         self.assertTrue(type(sddl) == LabeledColumn)
 
-        sw = self.v.simmat_words(['2','4','5'])
-        sd = self.v.simmat_docs([1, 0])
+        sw = self.v.dismat_word(['2','4','5'])
+        sd = self.v.dismat_doc([1, 0])
         self.assertTrue(type(sw) == IndexedSymmArray)
         self.assertTrue(type(sd) == IndexedSymmArray)
 
