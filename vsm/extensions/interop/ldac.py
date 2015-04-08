7b5e3dd9ff6f76a0d4cc48765bd5e9014ab17388
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-04-08 14:26:04 -0400

    expanding documentation

--- vsm/extensions/interop/ldac.py
+++ vsm/extensions/interop/ldac.py
@@ -1,7 +1,9 @@
 """
 vsm.extensions.interop.ldac
 
-
+Module containing functions for import/export between VSM and lda-c, which is
+the original LDA implementation referenced in Blei, Ng, and Jordan (2003). 
+lda-c is available at: http://www.cs.princeton.edu/~blei/lda-c/
 """
 import os, os.path
 
@@ -10,19 +12,28 @@ import numpy as np
 
 def export_corpus(corpus, outfolder, context_type='document'):
     """
-    Converts a vsm.corpus.Corpus object into a lda-c compatible corpus object.
-    [lda-c documentation](http://www.cs.princeton.edu/~blei/lda-c/readme.txt):
+    Converts a vsm.corpus.Corpus object into a lda-c compatible data file.
+    Creates two files:
+    1.  "vocab.txt" - contains the integer-word mappings
+    2.  "corpus.dat" - contains the corpus object in the format described in 
+        [lda-c documentation](http://www.cs.princeton.edu/~blei/lda-c/readme.txt):
 
-    Under LDA, the words of each document are assumed exchangeable.  Thus,
-    each document is succinctly represented as a sparse vector of word
-    counts. The data is a file where each line is of the form:
+            Under LDA, the words of each document are assumed exchangeable.  Thus,
+            each document is succinctly represented as a sparse vector of word
+            counts. The data is a file where each line is of the form:
+        
+                [M] [term_1]:[count] [term_2]:[count] ...  [term_N]:[count]
+        
+            where [M] is the number of unique terms in the document, and the
+            [count] associated with each term is how many times that term appeared
+            in the document.  Note that [term_1] is an integer which indexes the
+            term; it is not a string.
 
-        [M] [term_1]:[count] [term_2]:[count] ...  [term_N]:[count]
+    :param corpus: VSM Corpus object to convert to lda-c file
+    :type corpus: vsm.corpus.Corpus
 
-    where [M] is the number of unique terms in the document, and the
-    [count] associated with each term is how many times that term appeared
-    in the document.  Note that [term_1] is an integer which indexes the
-    term; it is not a string.
+    :param outfolder: Directory to output "vocab.txt" and "corpus.dat"
+    :type string: path
     """
     if not os.path.exists(outfolder):
         os.makedirs(outfolder)
@@ -45,7 +56,7 @@ def export_corpus(corpus, outfolder, context_type='document'):
             corpusfile.write("\n")
             
 
-def import_corpus(filename):
+def import_corpus(vocabfilename, corpusfilename):
     pass
 
 def import_model(filename):
