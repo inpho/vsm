369169ceee15babab8bb3d2e21cea765c8602d73
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-05-19 10:10:08 -0400

    updating TfMulti tests. addresses #91

--- unit_tests/tests_tf.py
+++ unit_tests/tests_tf.py
@@ -2,7 +2,31 @@ import unittest2 as unittest
 import numpy as np
 
 from vsm.model import tf
+from multiprocessing import Process
+import platform
 
+class MPTester:
+    def setUp(self):
+        self.corpus = np.array([0, 1, 3, 1, 1, 0, 3, 0, 3,
+                                3, 0, 1, 0,
+                                1, 3])
+        self.docs = [slice(0,9), slice(9,13),
+                     slice(13,13), slice(13,15)]
+        self.V = 4
+        self.cnt_mat = np.array([[3, 2, 0, 0],
+                                 [3, 1, 0, 1],
+                                 [0, 0, 0, 0],
+                                 [3, 1, 0, 1]])
+
+    def test_TfMulti_train(self):
+        self.setUp()
+        m = tf.TfMulti()
+        m.corpus = self.corpus
+        m.docs = self.docs
+        m.V = self.V
+        m.train(2)
+
+        assert (self.cnt_mat == m.matrix.toarray()).all()
 
 class TestTf(unittest.TestCase):
 
@@ -18,13 +42,12 @@ class TestTf(unittest.TestCase):
                                  [0, 0, 0, 0],
                                  [3, 1, 0, 1]])
         
-    def test_TF_train(self):
-        m = tf.TF()
-        m.corpus = self.corpus
-        m.docs = self.docs
-        m.V = self.V
-        m.train()
-        self.assertTrue((self.cnt_mat == m.matrix.toarray()).all())
+    def test_TF_proper_class(self):
+        m = tf.TF(multiprocessing=True)
+        if platform.system() == 'Windows':
+            self.assertTrue(isinstance(m,tf.TfSeq))
+        else:
+            self.assertTrue(isinstance(m,tf.TfMulti))
 
     def test_TfSeq_train(self):
         m = tf.TfSeq()
@@ -34,19 +57,15 @@ class TestTf(unittest.TestCase):
         m.train()
         self.assertTrue((self.cnt_mat == m.matrix.toarray()).all())
 
-    '''
-    def test_TfMulti_train(self):
-        m = tf.TfMulti()
-        m.corpus = self.corpus
-        m.docs = self.docs
-        m.V = self.V
-        m.train(2)
-
-        self.assertTrue((self.cnt_mat == m.matrix.toarray()).all())
-    '''
+    def test_demo_TfMulti_train(self):
+        t = MPTester()
+        p = Process(target=t.test_TfMulti_train, args=())
+        p.start()
+        p.join()
 
 
         
 #Define and run test suite
-suite = unittest.TestLoader().loadTestsFromTestCase(TestTf)
-unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTf)
+    unittest.TextTestRunner(verbosity=2).run(suite)
