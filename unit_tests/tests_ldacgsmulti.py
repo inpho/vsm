990482c1dd99e87354d671d5a122d698cff1bf90
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-06-07 21:28:23 -0400

    adding unittests for sequential bug

--- unit_tests/tests_ldacgsmulti.py
+++ unit_tests/tests_ldacgsmulti.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from vsm.corpus import Corpus
 from vsm.corpus.util.corpusbuilders import random_corpus
+from vsm.model.ldacgsseq import *
 from vsm.model.ldacgsmulti import *
 from multiprocessing import Process
 
@@ -170,6 +171,78 @@ class MPTester:
         assert getattr(m0, 'seed', None) is None
         assert getattr(m0, '_mtrand_state', None) is None
 
+    def test_LdaCgsMulti_eq_LdaCgsSeq(self):
+        from tempfile import NamedTemporaryFile
+        import os
+    
+        c = random_corpus(1000, 50, 6, 100, seed=2)
+        tmp = NamedTemporaryFile(delete=False, suffix='.npz')
+        m0 = LdaCgsMulti(c, 'document', K=10, n_proc=1, seeds=[2])
+        m1 = LdaCgsSeq(c, 'document', K=10, seed=2)
+        for iteration in range(20):
+            m0.train(n_iterations=1, verbose=0)
+            m1.train(n_iterations=1, verbose=0)
+            
+            assert m0.context_type == m1.context_type
+            assert m0.K == m1.K
+            assert (m0.alpha == m1.alpha).all()
+            assert (m0.beta == m1.beta).all()
+            for i in xrange(max(len(m0.corpus), len(m1.corpus))):
+                assert m0.corpus[i].all() == m1.corpus[i].all()
+            assert m0.V == m1.V
+            assert m0.iteration == m1.iteration
+            assert (m0.Z[i] == m1.Z[i]).all()
+            assert (m0.top_doc == m1.top_doc).all()
+            assert (m0.word_top == m1.word_top).all()
+            assert (np.isclose(m0.inv_top_sums, m1.inv_top_sums)).all()
+    
+            assert m0.seeds[0] == m1.seed
+            assert m0._mtrand_states[0][0] == m1._mtrand_state[0]
+            for s0,s1 in zip(m0._mtrand_states[0][1], m1._mtrand_state[1]):
+                assert s0 == s1
+            assert m0._mtrand_states[0][2] == m1._mtrand_state[2]
+            assert m0._mtrand_states[0][3] == m1._mtrand_state[3]
+            assert m0._mtrand_states[0][4] == m1._mtrand_state[4]
+            print iteration, m0.log_probs[-1], m1.log_probs[-1] 
+            for i in range(iteration):
+                assert np.isclose(m0.log_probs[i][1], m1.log_probs[i][1])
+    
+    def test_LdaCgsMulti_eq_LdaCgsSeq_multi(self):
+        from tempfile import NamedTemporaryFile
+        import os
+    
+        c = random_corpus(1000, 50, 6, 100, seed=2)
+        tmp = NamedTemporaryFile(delete=False, suffix='.npz')
+        m0 = LdaCgsMulti(c, 'document', K=10, n_proc=1, seeds=[2])
+        m1 = LdaCgsSeq(c, 'document', K=10, seed=2)
+        for iteration in range(20):
+            m0.train(n_iterations=2, verbose=0)
+            m1.train(n_iterations=2, verbose=0)
+            
+            assert m0.context_type == m1.context_type
+            assert m0.K == m1.K
+            assert (m0.alpha == m1.alpha).all()
+            assert (m0.beta == m1.beta).all()
+            for i in xrange(max(len(m0.corpus), len(m1.corpus))):
+                assert m0.corpus[i].all() == m1.corpus[i].all()
+            assert m0.V == m1.V
+            assert m0.iteration == m1.iteration
+            assert (m0.Z[i] == m1.Z[i]).all()
+            assert (m0.top_doc == m1.top_doc).all()
+            assert (m0.word_top == m1.word_top).all()
+            assert (np.isclose(m0.inv_top_sums, m1.inv_top_sums)).all()
+    
+            assert m0.seeds[0] == m1.seed
+            assert m0._mtrand_states[0][0] == m1._mtrand_state[0]
+            for s0,s1 in zip(m0._mtrand_states[0][1], m1._mtrand_state[1]):
+                assert s0 == s1
+            assert m0._mtrand_states[0][2] == m1._mtrand_state[2]
+            assert m0._mtrand_states[0][3] == m1._mtrand_state[3]
+            assert m0._mtrand_states[0][4] == m1._mtrand_state[4]
+            print iteration, m0.log_probs[-1], m1.log_probs[-1] 
+            for i in range(iteration):
+                assert np.isclose(m0.log_probs[i][1], m1.log_probs[i][1])
+
 
 class TestLdaCgsMulti(unittest.TestCase):
     def setUp(self):
@@ -210,6 +283,19 @@ class TestLdaCgsMulti(unittest.TestCase):
         p = Process(target=t.test_LdaCgsMulti_continue_training, args=())
         p.start()
         p.join()
+    
+    def test_LdaCgsMulti_eq_LdaCgsSeq(self):
+        t = MPTester()
+        p = Process(target=t.test_LdaCgsMulti_eq_LdaCgsSeq, args=())
+        p.start()
+        p.join()
+    
+    def test_LdaCgsMulti_eq_LdaCgsSeq_multi(self):
+        t = MPTester()
+        p = Process(target=t.test_LdaCgsMulti_eq_LdaCgsSeq_multi, args=())
+        p.start()
+        p.join()
+    
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestLdaCgsMulti)
