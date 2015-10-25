c8cc9e923d50cecb365c6d549778842aa1a7d98d
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-10-25 19:01:31 -0400

    adding alpha 1 tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ packages.append('vsm')
 
 setup(
     name = "vsm",
-    version = "0.3.8",
+    version = "0.4.0a1",
     description = ('Vector Space Semantic Modeling Framework '\
                    'for the Indiana Philosophy Ontology Project'),
     author = "The Indiana Philosophy Ontology (InPhO) Project",
