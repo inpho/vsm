2978901128cc1e04fe490bfdbc7f0a68ac0614b6
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-14 17:13:31 -0400

    Version bump for tons of stoplist fixes

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires=[
 
 setup(
     name = "vsm",
-    version = "0.4.0a22",
+    version = "0.4.0a23",
     description = ('Vector Space Semantic Modeling Framework '\
                    'for the Indiana Philosophy Ontology Project'),
     author = "The Indiana Philosophy Ontology (InPhO) Project",
