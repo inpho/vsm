d8b4f7af724134ead4175dcd3029ba77df70ccf2
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-28 12:52:00 -0400

    fixing unicode error in ldasentences

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires=[
 
 setup(
     name = "vsm",
-    version = "0.4.0a25",
+    version = "0.4.0a26",
     description = ('Vector Space Semantic Modeling Framework '\
                    'for the Indiana Philosophy Ontology Project'),
     author = "The Indiana Philosophy Ontology (InPhO) Project",
