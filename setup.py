38f129c929a0cda9d1c5fc188857f0bd2f8fdd9e
Jaimie Murdock <jaimie.murdock@gmail.com>
2019-04-18 00:35:51 -0400

    version bump for compatibility issues

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if platform.python_version_tuple()[0] == '2':
 
 setup(
     name = "vsm",
-    version = "0.4.13",
+    version = "0.4.14",
     description = ('Vector Space Semantic Modeling Framework '\
                    'for the Indiana Philosophy Ontology Project'),
     author = "The Indiana Philosophy Ontology (InPhO) Project",
